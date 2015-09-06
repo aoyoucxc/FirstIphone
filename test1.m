9681f17ff500b4fac411fde0abe7c27ff18eee86
CXC <CXC@CinusS-MBP.local>
2015-09-06 15:14:40 +0900

    hello

---
+++ test1.m
@@ -0,0 +1,18 @@
+//
+//  test1.m
+//  FirstIphone
+//
+//  Created by CXC on 15/9/6.
+//  Copyright (c) 2015年 CXC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int arc,const char * argv[]){
+    
+    @autoreleasepool {
+        NSLog(@"Hello,World!!");
+    }
+    
+    return 0;
+}
